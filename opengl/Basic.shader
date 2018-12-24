466fe36db0d1d9011f2154fcd24f0e7d75907d45
doublesand <doublesand@163.com>
2018-12-24 23:25:55 +0800

    调试

--- opengl/Basic.shader
+++ opengl/Basic.shader
@@ -1,6 +1,8 @@
 #shader vertex
 #version 330 core
+
 layout(location = 0) in vec4 position;
+
 void main()
 {
 	gl_Position = position;
@@ -8,8 +10,12 @@ void main()
 
 #shader fragment
 #version 330 core
+
+//C++中传来的数据
+uniform vec4 u_Color;
+
 layout(location = 0) out vec4 color;
 void main()
 {
-	color = vec4(0.5, 0.6, 0.7, 1.0);
+	color = u_Color;
 };
--- opengl/main.cpp
+++ opengl/main.cpp
@@ -12,6 +12,7 @@
 	ASSERT(GLLogCall(#x, __FILE__, __LINE__))
 	
 
+
 static void GLClearError() {
 	while (glGetError() != GL_NO_ERROR);
 }
@@ -106,6 +107,8 @@ int main(void)
 	if (!glfwInit())
 		return -1;
 
+
+
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	if (!window)
@@ -116,6 +119,8 @@ int main(void)
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+
+	glfwSwapInterval(1);
 
 	if (glewInit() != GLEW_OK)  //在上面那条语句下开始
 		std::cout << "ERROR!" << std::endl;
@@ -136,10 +141,11 @@ int main(void)
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);  //申请GPU内存
 	glBindBuffer(GL_ARRAY_BUFFER, buffer); //绑定内存
-	glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_STATIC_DRAW); //传入数据
-
+	glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW); //传入数据
+	
+	//开启并声明顶点属性，以至于着色器可以访问
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0); //确定顶点属性
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0); //确定正方形的顶点属性
 
 	unsigned int ibo;
 	glGenBuffers(1, &ibo);  //申请GPU内存
@@ -150,15 +156,29 @@ int main(void)
 	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
 	glUseProgram(shader);
 
+	//开启着色器之后使用uniform给着色器传变量
+	int location = glGetUniformLocation(shader, "u_Color");
+	ASSERT(location != -1); //如果是-1说明着色器没有这个变量
+	
+	float r = 0.0f; //红色分量渐变色
+	float increment = 0.05f;  //颜色步长
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glUniform4f(location, r, 0.6f, 0.1f, 1.0f);
+
 		//第一次传入的索引可以找到数组里的顶点并绘制，根据绘制的类型找到顶点数据
 
-		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr));
+		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+		if (r > 1.0f)
+			increment = -0.05f;
+		else if (r < 0.0f)
+			increment = 0.05f;
+		r += increment;
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
